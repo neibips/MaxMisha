d591361b8bff930c047cee14047f1e3e1749f114
maxclus1on <max.bolovin@mail.ru>
2020-07-25 14:05:02 +0500

    dfsfs

--- python.py
+++ python.py
@@ -1,7 +1,11 @@
-hello = input()
-print(hello)
-print('hello world')
-ddos = input('print')
-fuck = input()
-nigga = input()
-gavnicee
+value = (input("Enter a value you want to convert: "))
+
+if value == "km":
+    value_km = input("Enter amount of km you want to convert into miles: ")
+elif value == "mile":
+    value_mile = input("Enter amount of miles you want to convert into km: ")
+else:
+    print("Type the correct form")
+
+def convert_km (value_km):
+  return value_km * 1.60934
